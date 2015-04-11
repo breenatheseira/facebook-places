4b2a04f4c07c2e4d6b81f584d7ba65c0e2f2b979
Breena Theseira <breenatheseira@yahoo.com>
2015-04-11 17:57:53 +0800

    Updated: Removed most of the unused pages, and created a new search page.

--- app/views/places/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @place, :id, :business, :area, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :places
+  # resources :places
+
+  # post
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'places#index'
+  root 'places#search'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
